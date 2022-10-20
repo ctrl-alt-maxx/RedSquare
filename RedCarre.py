cb60e97cd4f1296bb6b0bfc277d9b0b7501e98b7
LayzyNoob <78440748+LayzyNoob@users.noreply.github.com>
2022-10-20 18:50:52 -0400

    Update RedCarre.py
    Choisir une difficulté déplace le rectangle d'une certaine vitesse.

--- RedCarre.py
+++ RedCarre.py
@@ -1,11 +1,11 @@
 from functools import partial
 import tkinter as tk
-from tkinter import ANCHOR, CENTER, Canvas, Label, Menu, StringVar
-from turtle import width
+from tkinter import CENTER, Canvas, Label, Menu
 import c31Geometry2 as c31
 
-
+go = bool(True)
 root = tk.Tk()
+# RAPPROCHE LE CURSEUR DU CARRÉ
 root.state('zoomed')
 
 # Background color
@@ -21,6 +21,26 @@ root.geometry("1000x1000")
 # Creating canvas 
 canvas = tk.Canvas(root, background="white", width=450, height=450, highlightthickness=50, highlightbackground="black")
 
+# DETERMINE LA VITTESE DES FORMES BLEUES AU DÉBUT DU JEU
+class Difficulté:
+    defaultSpeed = 2000
+    def Facile(self):
+        self.defaultSpeed = 1000
+        self.color = "red"
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+
+    def Moyen(self):
+        self.defaultSpeed = 500
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+        
+    def Difficile(self):
+        self.defaultSpeed = 100
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+      
+d = Difficulté()
 
 #canvas = c31.Carre(root, c31.Vecteur(450,450), 450, bordure="black", remplissage="white", epaisseur=50)
 
@@ -51,8 +71,8 @@ def log(var, e):
 #carre = c31.Carre(canvas, c31.Vecteur(225, 225), 40, remplissage="red")
 #carre.draw()
 
-loop = c31.LoopEvent(root, partial(log, "Cliquez-Loop", None), 1000)
-loop.start()
+# loop = c31.LoopEvent(root, partial(log, "Cliquez-Loop", None), 1000)
+# loop.start()
 
 def mvt(forme):
     forme.translate(c31.Vecteur(4, 0))
@@ -80,9 +100,9 @@ label.pack()
 popup = Menu(root, tearoff=0)
 
 # Adding items to the menu 
-popup.add_command(label="Facile")
-popup.add_command(label="Moyen")
-popup.add_command(label="Difficile")
+popup.add_command(label="Facile", command=d.Facile)
+popup.add_command(label="Moyen", command=d.Moyen)
+popup.add_command(label="Difficile", command=d.Difficile)
 popup.add_separator()
 popup.add_command(label="Quitter", command=root.destroy)
 
@@ -101,14 +121,21 @@ root.bind("<ButtonPress-1>", partial(print, "pressed"))
 root.bind("<ButtonRelease>", partial(print, "released"))
 root.bind("<B1-Motion>", partial(print, "dragged"))
 
-def Facile():
-    speed = 50;
+def mvt(forme): forme.translateTo(c31.Vecteur(300,95))
 
-def Moyen():
-    speed = 30
+def update():
+    rectangle2.draw()
+
+# loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), 300)
+
+if rectangle2.get_position() == c31.Vecteur(300,95) :
+    go = bool(False)
     
-def Difficile():
-    speed = 10        
+if go == bool(True) : 
+    loop.start()
+    
+e = c31.LoopEvent(root, update, d.defaultSpeed)
+e.startImmediately()
 
 def drag(event) :
     event.widget.place(x=event.x_root, y=event.y_root, anchor=CENTER)
@@ -117,5 +144,6 @@ carre2 = Canvas(root, width=40, height=40, bg="red")
 carre2.place(x=225, y=225, anchor=CENTER)
 carre2.bind("<B1-Motion>", drag)
 
+# CACHE LE CURSEUR SUR LE CARREÉ ROUGE
 carre2.config(cursor="none")
 root.mainloop()
