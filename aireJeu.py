9ca64b252aaad46a2fd78ee71dba531756758167
usha.s <ushasookwa@gmail.com>
2022-11-01 18:11:21 -0400

    rien

---
+++ aireJeu.py
@@ -0,0 +1,169 @@
+#import vueMenu as menu
+from cmath import rect
+from functools import partial
+from math import dist
+import tkinter as tk
+from tkinter import BOTH, Canvas, StringVar, Variable
+#import os, sys, tkinter.filedialog
+import c31Geometry2 as c31
+from tkinter import Menu
+
+root = tk.Tk()
+
+# Couleur de fond 
+root.config(background="LightSkyBlue1")
+
+# Ajouter un titre à la fenêtre Tk
+root.title("Jeu du carré rouge")
+
+# Fixe la taille de la fenêtre en px
+root.geometry("850x850")
+
+
+# Création d'un menuBar
+menubar = Menu(root)
+root.config(menu=menubar)
+
+# Creation du file_menu 
+file_menu = Menu(
+    menubar,
+    tearoff=0
+)
+
+# Ajouter des items dans le menu 
+file_menu.add_command(label='Facile')
+file_menu.add_command(label='Moyen')
+file_menu.add_command(label='Difficle')
+file_menu.add_separator()
+file_menu.add_command(label='Quitter', command=root.destroy)
+
+# Ajouter le file_menu au menuBar
+menubar.add_cascade(
+    label="Menu",
+    menu = file_menu
+)
+
+# Avoir un fichier excutable 
+#pyExec = sys.executable
+
+# En cliquant sur une option, une nouvelle fenêtre s'affiche
+#def open():
+#    if (menu.file_menu != "Quitter") :
+#        print("not quit")
+#        PathPyFile = tkinter.filedialog.askopenfilename(title="Open a file",filetypes=[('vueMenu','.py')])
+#        os.system('%s %s' % (pyExec, PathPyFile))
+
+#open()
+
+# Création du canvas 
+canvas = tk.Canvas(root, background="white", width=450, height=450, highlightthickness=50, highlightbackground="black")
+rectYellow = c31.Rectangle(canvas, c31.Vecteur(450, 450), 850, 850, remplissage="yellow")
+rectYellow.draw()
+
+# Création des rectangles
+# 1. Rectangle bleu gauche
+rectangle1 = c31.Rectangle(canvas, c31.Vecteur(100, 100), 60, 60, remplissage="mediumblue")
+rectangle1.draw()
+
+# 2. Rectangle bleu superieur droit
+rectangle2 = c31.Rectangle(canvas, c31.Vecteur(300, 85), 60, 50, remplissage="mediumblue")
+rectangle2.draw()
+
+# 3. Rectangle bleu inferieur gauche 
+rectangle3 = c31.Rectangle(canvas, c31.Vecteur(85, 350), 30, 60, remplissage="mediumblue")
+rectangle3.draw()
+
+# 4. Rectangle bleu infereieur droit 
+rectangle4 = c31.Rectangle(canvas, c31.Vecteur(355, 340), 100, 20, remplissage="mediumblue")
+rectangle4.draw()
+
+canvas.pack()
+
+# Création du carré rouge 
+carreRouge = Canvas(root, width=40, height=40, background="red")
+carreRouge.place(x=420, y=250, anchor=tk.CENTER)
+
+# Fonction pour glisser (drag) carré rouge 
+def glisser(event) :
+    event.widget.place(x=event.x_root, y=event.y_root, anchor=tk.CENTER)
+
+
+carreRouge.bind("<B1-Motion>", glisser)
+
+print("Rectangle1", c31.Rectangle.get_coordonnees(rectangle1))
+print("rectJaune", c31.Rectangle.get_coordonnees(rectYellow))
+
+#carre = c31.Carre(canvas, c31.Vecteur(270, 249), 40, remplissage="green")
+#carre.draw()
+
+#carre.bind("<B1-Motion>", glisser)
+
+#print("Carre", c31.Carre.get_coordonnees(carre))
+#print("CarreRougeCanvas", canvas.coords(carreRouge))
+
+#def move() :
+#    while (carreRouge.bind("<B1-Motion>", glisser)) :
+#        if (carreRouge.place(x=879)) :
+#            print("out")
+
+#var = StringVar()
+#def outsideYellowRect(event) :
+#    var.set("is outside")
+
+def mvt(forme) :
+    forme.translate(c31.Vecteur(10,0)) 
+    forme.draw()
+
+def mvtBack(forme) :
+    forme.translate(c31.Vecteur(8,0))
+    forme.translate(c31.Vecteur(0,9))
+    forme.draw()
+
+go = bool(True)
+
+loop = c31.LoopEvent(canvas, partial(mvt, rectangle1))
+loop.start()
+
+def mouseDragged() :
+    root.bind("<B1-Motion>", partial(print, "dragged"))
+    carreRouge.bind("<B1-Motion>", glisser)
+
+mouseDragged()
+
+# CACHE LE CURSEUR SUR LE CARREÉ ROUGE
+carre2.config(cursor="none")
+
+# DETERMINE LA VITTESE DES FORMES BLEUES AU DÉBUT DU JEU
+class Difficulté:
+    defaultSpeed = 0 # THE LOWER THE VALUE, THE CLEANNER THE MOVEMENT WITH FASTER SPEED
+    def Facile(self):
+        self.defaultSpeed = 1000
+        self.color = "red"
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+
+    def Moyen(self):
+        self.defaultSpeed = 200
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+        
+    def Difficile(self):
+        self.defaultSpeed = 50
+        loop = c31.LoopEvent(canvas,partial(mvt,rectangle2), self.defaultSpeed)
+        loop.start()
+      
+d = Difficulté()
+
+def update():
+    rectangle2.draw()
+
+if rectangle2.get_position() == c31.Vecteur(300,95) :
+    go = bool(False)
+    
+if go == bool(True) : 
+    loop.start()
+    
+e = c31.LoopEvent(root, update, d.defaultSpeed)
+e.startImmediately()
+
+root.mainloop()
