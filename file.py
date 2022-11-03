3254b429d2b08aea353a01508db0f4e295e84b7c
LayzyNoob <78440748+LayzyNoob@users.noreply.github.com>
2022-11-02 20:44:12 -0400

    File Complete

--- file.py
+++ file.py
@@ -1,56 +1,48 @@
-from ctypes.wintypes import SIZE
-from time import time
 import tkinter as tk
-from tkinter import filedialog as fd
 from tkinter import messagebox as msg
 import datetime as dt
 import csv
 import random
-from tracemalloc import start
-
 
 root = tk.Tk()
-root.title('Tkinter File')
-root.resizable(False, False)
+root.title('Tkinter Game')
+# root.resizable(False, False)
 root.geometry('800x800')
 d = dt.datetime.now()
 # fd.askopenfilename(initialdir="x:/C31-Genie Logiciel/carreRouge/wow.txt", title="Open Text File", filetypes=(("Text Files", "*.txt"),))
 
-# name = "Marc"
-# t = random.randrange(1,21)
-# f = open("x:/C31-Genie Logiciel/carreRouge/wow.csv", "a")
-# #print(f.read())
-# with f:
-#     fnames = ['Nom', 'Temps', 'Annee', 'Mois', 'Jour']
-#     csvwriter = csv.DictWriter(f, fieldnames=fnames)
-#     csvwriter.writerow( {'Nom' : name, 'Temps' : t, 'Annee' : d.year, 'Mois' : d.month, 'Jour' : d.day} ) 
-# f.close()
+name = "Marc"
+t = random.randrange(1,21)
+f = open("score.txt", "a")
+#print(f.read())
+with f:
+    fnames = ['Nom', 'Temps', 'Date Joué']
+    csvwriter = csv.DictWriter(f, fieldnames=fnames)
+    csvwriter.writerow( {'Nom' : name, 'Temps' : t, 'Date Joué' : d} ) 
+f.close()
 
 answer = msg.askyesno("Question","Do want to see other scores?")
 
-
-# print(answer)
+#print(answer)
 if answer == True :
-    col_names = ("Username", "Name", "Date Created","i","j")
-    for i, col_name in enumerate(col_names, start=1):
-        tk.Label(root, text=col_name).grid(row=3, column=i, padx=40)
+    score = tk.Tk()
+    score.title('Tkinter Scores')
+    score.resizable(True, False)
+    score.geometry('800x400')
+    
+    col_names = ("Username", "Temps","Date Joué")
+    for i, col_name in enumerate(col_names, start=0):
+        tk.Label(score, text=col_name).grid(row=0, column=i, padx=40)
         
     # f = open()    
-    with open("wow.txt", "r", newline="") as scorefile:
+    with open("score.txt", "r", newline="\n") as scorefile:
         reader = csv.reader(scorefile)
         data = list(reader)
         
     scoreslist = []
-    for i, row in enumerate(data, start=4):
+    for i, row in enumerate(data):
         scoreslist.append(row[0])
-        
-        for col in range(1,8):
-            tk.Label(root, text=row[col]).grid(row=i, column=col)
-        
-    count = 0
-    while count <= len(scorefile):
-        print(scorefile[count])
-        count += 1
-
+        for col in range(0, 3):
+            tk.Label(score, text=row[col]).grid(row=i+1, column=col, padx=100)
 
 root.mainloop()
