5fbf1f942fa229559bedc6787e97c03db67802a2
usha.s <ushasookwa@gmail.com>
2022-11-03 20:27:10 -0400

    updated

--- CarreRougeFinale.py
+++ CarreRougeFinale.py
@@ -116,80 +116,103 @@ instruction()
 
 # CHAQUE ENEMIE À SON PROPRE MOUVEMENT (le sens de direction)
 def mvt1(forme): 
-    forme.translate(c31.Vecteur(2,0)) 
-    forme.translate(c31.Vecteur(0,2)) 
+    forme.translate(c31.Vecteur(2,2)) 
     en1.draw()
                 
 def mvt2(forme): 
     forme.translate(c31.Vecteur(0,2)) 
-    forme.translate(c31.Vecteur(0,2)) 
     en2.draw()
 
 def mvt3(forme): 
-    forme.translate(c31.Vecteur(10,0)) 
-    forme.translate(c31.Vecteur(10,0)) 
+    forme.translate(c31.Vecteur(4,-2))  
     en3.draw()
                 
 def mvt4(forme): 
-    forme.translate(c31.Vecteur(0,-8)) 
-    forme.translate(c31.Vecteur(-8,0)) 
+    forme.translate(c31.Vecteur(0,-4)) 
+    forme.translate(c31.Vecteur(-4,0)) 
     en4.draw() 
 
 # EXÉCUTE APRÈS UN CERTAIN TEMPS
 
-loop1 = c31.LoopEvent(canvasBase, partial(mvt1, en1)) 
+loop1 = c31.LoopEvent(canvasBase, partial(mvt1, en1), 50) 
 loop1.start()
 
-loop2 = c31.LoopEvent(canvasBase, partial(mvt2, en2))
+loop2 = c31.LoopEvent(canvasBase, partial(mvt2, en2), 50)
 loop2.start()
 
-loop3 = c31.LoopEvent(canvasBase, partial(mvt3, en3))
+loop3 = c31.LoopEvent(canvasBase, partial(mvt3, en3), 50)
 loop3.start()
             
-loop4 = c31.LoopEvent(canvasBase, partial(mvt4, en4))
+loop4 = c31.LoopEvent(canvasBase, partial(mvt4, en4), 50)
 loop4.start()
 
-def enregistrerScores():
-    d = dt.datetime.now()
-    t = random.randrange(1, 21)
-    name = "Marc"
-
-    # FICHIER CONTENANT LES TEMPS DES PARTIES PRÉCEDENTES
-    f = open("scores.csv", "a")
-    with f:
-        fnames = ['Nom', 'Temps', 'Date Joué']
-        csvwriter = csv.DictWriter(f, fieldnames=fnames)
-        csvwriter.writerow( {'Nom' : name, 'Temps' : t, 'Date Joué' : d} ) 
-    f.close()
-
-def montrerScores():
-    # RÉPONSE POUR SI L'UTILISATEUR VEUT VOIR LES TEMPS PRÉCEDENTS
-    reponse = msg.askyesno("Question","Voulez-vous voir le temps des parties précédentes?")
-
-    if reponse == True :
-        score = tk.Tk()
-        score.title('Tkinter Scores')
-        score.geometry('800x400')
+def Jeu():
+    while True:
+        anon = msg.askyesno("Question", "Voulez-vous mettre votre nom?")
+        d = dt.datetime.now()
+        if anon == False:
+            name = "Joueur Anonyme"
+        else:
+            # time.sleep(2)
+            id = tk.Tk()
+            id.title("Identifiant")
+            id.resizable(False, False)
+            id.geometry("300x200")
+            tk.Label(id, text="Écrie ton nom").grid(row=0)
+            e = tk.Entry(id)
+            e.grid(row=0, column=1)
             
-        col_noms = ("Username", "Temps", "Date Joué")
-        for i, col_name in enumerate(col_noms, start=0):
-            tk.Label(score, text=col_name).grid(row=0, column=i, padx=40)
+            tk.Button(id, text="Enregistrer", command=id.quit).grid(row=3, column=0)
+            id.mainloop()
+            name = e.get()
+            id.destroy()
+            
+        # name = "Marc"
+        t = random.randrange(1,21)
+        # FICHIER CONTENANT LES TEMPS DES PARTIES PRÉCEDENTES
+        f = open("score.csv", "a")
+        with f:
+            fnames = ['Nom', 'Temps', 'Date']
+            csvwriter = csv.DictWriter(f, fieldnames=fnames)
+            csvwriter.writerow( {'Nom' : name, 'Temps' : t, 'Date' : d} ) 
+        f.close()
+
+        # RÉPONSE POUR SI LA PERSONNE VEUT VOIR LES TEMPS PRÉCEDENTS
+        answer = msg.askyesno("Question","Voulez-vous voir le temps des autres parties?")
+        
+        if answer == True :
+            score = tk.Tk()
+            score.title('Tkinter Scores')
+            # score.resizable(True, True)
+            score.geometry('800x400')
+            
+            col_names = ("Joueur", "Temps Survécu (secondes)", "Date Joué")
+            for i, col_name in enumerate(col_names, start=0):
+                tk.Label(score, text=col_name).grid(row=0, column=i, padx=40)
                 
-        # ACCÈS AU FICHIER
-        with open("scores.csv", "r", newline="\n") as scorefile:
-            reader = csv.reader(scorefile)
-            data = list(reader)
+            # ACCÈS AU FICHIER
+            with open("score.csv", "r", newline="\n") as scorefile:
+                reader = csv.reader(scorefile)
+                data = list(reader)
                 
-        scoresliste = []
-        for i, row in enumerate(data):
-            scoresliste.append(row[0])
-            for col in range(0, 3):
-                tk.Label(score, text=row[col]).grid(row=i+1, column=col, padx=100)
-                    
-        score.mainloop()
+            scoreslist = []
+            for i, row in enumerate(data):
+                scoreslist.append(row[0])
+                for col in range(0, 3):
+                    tk.Label(score, text=row[col]).grid(row=i+1, column=col, padx=100)      
+            score.mainloop()        
+                            
+        # RÉPONSE POUR SI LA PERSONNE VEUT FAIRE UNE AUTRE PARTIE
+        answer2 = msg.askyesno("Rejouer", "Voulez-vous rejoué?")
+        root.mainloop()
+        if answer2 == False:
+            root.destroy()
+        else:
+            Jeu()
+
     
 # PERMET DE DÉTRUIRE LE WINDOW ROOT 
-root.after(3099, root.destroy)
+root.after(1099, root.destroy)
 
 root.mainloop()
 
